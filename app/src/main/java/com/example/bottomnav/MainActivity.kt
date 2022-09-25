3004bc034b60993e816cb43228455ffd90a94276
Mansur <mahcypc@mail.ru>
2022-09-26 01:23:37 +0300

    Week2 Lesson1

---
+++ app/src/main/java/com/example/bottomnav/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.bottomnav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bottomnav.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
+        binding?.nav?.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
+                R.id.catalogItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Catalog()).commit()
+                R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Delivery()).commit()
+                R.id.contactsItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Contacts()).commit()
+
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+}
